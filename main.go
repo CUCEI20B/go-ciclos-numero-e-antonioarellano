dae4e4b85ffae781254f8ddd73ecafc7834c9ab8
Antonio Arellano <jose.arellano6698@alumnos.udg.mx>
2020-10-01 01:38:25 -0500

    Funcion para obtener e segun el limite dado

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var x, e, i float64
+	i = 0
+	fmt.Scanln(&x)
+	for i <= x {
+		e = e + (1 / factorial(i))
+		i++
+	}
+	fmt.Println(e)
+}
+
+func factorial(n float64) float64 {
+	if n < 0 {
+		return 0
+	} else {
+		if n > 1 {
+			return n * factorial(n-1)
+		}
+		return 1
+	}
 }
